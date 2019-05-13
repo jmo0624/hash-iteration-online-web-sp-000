e1683b0a2935963124f2517103a97ba9ebd353cc
Josh Morris <jmo0624@gmail.com>
2019-05-13 23:07:58 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,5 +13,8 @@ end
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age >= 12
-      puts "Happy Birthday #{name}!"
+      puts "Happy Birthday #{name}! You are now #{age} years old!"
+    end
+  end
+end
 
